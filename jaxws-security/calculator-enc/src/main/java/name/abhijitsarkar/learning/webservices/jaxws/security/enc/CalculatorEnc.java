ca3bcfbd69483c5b29be8fc8ba54f9da7fc98972
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2013-11-04 19:16:08 -0500

    Cleaned up

--- jaxws-security/calculator-enc/src/main/java/name/abhijitsarkar/learning/webservices/jaxws/security/enc/CalculatorEnc.java
+++ jaxws-security/calculator-enc/src/main/java/name/abhijitsarkar/learning/webservices/jaxws/security/enc/CalculatorEnc.java
@@ -1,12 +1,10 @@
 package name.abhijitsarkar.learning.webservices.jaxws.security.enc;
 
-import javax.ejb.Stateless;
 import javax.jws.HandlerChain;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
-@Stateless(name = "CalculatorEnc")
 @WebService(portName = "CalculatorEnc", name = "CalculatorEnc", serviceName = "CalculatorEncService", targetNamespace = "http://abhijitsarkar.name/learning/webservices/jaxws/security/")
 @HandlerChain(file = "jaxws-handler-chains.xml")
 public class CalculatorEnc {
--- jaxws-security/calculator-ut/src/main/java/name/abhijitsarkar/learning/webservices/jaxws/security/ut/CalculatorUT.java
+++ jaxws-security/calculator-ut/src/main/java/name/abhijitsarkar/learning/webservices/jaxws/security/ut/CalculatorUT.java
@@ -1,12 +1,10 @@
 package name.abhijitsarkar.learning.webservices.jaxws.security.ut;
 
-import javax.ejb.Stateless;
 import javax.jws.HandlerChain;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
-@Stateless(name = "CalculatorUT")
 @WebService(portName = "CalculatorUT", name = "CalculatorUT", serviceName = "CalculatorUTService", targetNamespace = "http://abhijitsarkar.name/learning/webservices/jaxws/security/")
 @HandlerChain(file = "jaxws-handler-chains.xml")
 public class CalculatorUT {
