75f49a3c83be9023faf345006efabcaa17d5eaa2
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2013-11-08 03:38:32 -0500

    Messed up

--- jwsur2/jwsur2-ch5/src/main/java/com/github/mkalin/jwsur2/ch5/images/base64/client/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://base64.images.ch5.jwsur2.mkalin.github.com/")
-package com.github.mkalin.jwsur2.ch5.images.base64.client.generated;
--- jwsur2/jwsur2-ch5/src/main/java/com/github/mkalin/jwsur2/ch5/predictions/client/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://predictions.ch5.jwsur2.mkalin.github.com/")
-package com.github.mkalin.jwsur2.ch5.predictions.client.generated;
