5007e5045b3d874fde6b711f2e827c2e6e2075d8
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2013-11-09 00:32:38 -0500

    Clean up

--- calculator-ssl-client/src/main/java/edu/certification/abhijitsarkar/ocewsd/wssecurity/calculatorssl/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://calculatorssl.wssecurity.ocewsd.abhijitsarkar.certification.edu/")
-package edu.certification.abhijitsarkar.ocewsd.wssecurity.calculatorssl;
