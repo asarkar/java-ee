b9de6c41972f954c93bff7bc9c5c49584f1a9c8e
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2013-11-24 02:24:48 -0500

    Deleted jwsur1 code

--- webservices/martinkalin/src/main/java/edu/certification/abhijitsarkar/ocewsd/martinkalin/ch03/image/mtom/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://mtom.image.ch03.martinkalin.ocewsd.abhijitsarkar.certification.edu/")
-package edu.certification.abhijitsarkar.ocewsd.martinkalin.ch03.image.mtom;
--- webservices/martinkalin/src/main/java/edu/certification/abhijitsarkar/ocewsd/martinkalin/ch03/image/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://image.ch03.martinkalin.ocewsd.abhijitsarkar.certification.edu/")
-package edu.certification.abhijitsarkar.ocewsd.martinkalin.ch03.image;
