7b319c0833d1dba07f6e3d151227726766064d1a
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2013-11-25 20:29:02 -0500

    Added code to demo basic auth using Authenticator

--- webservices/jax-ws/jaxws-security/jaxws-security-client/src/main/java/name/abhijitsarkar/webservices/jaxws/security/client/CalculatorResource.java
+++ webservices/jax-ws/jaxws-security/jaxws-security-client/src/main/java/name/abhijitsarkar/webservices/jaxws/security/client/CalculatorResource.java
@@ -1,5 +1,8 @@
 package name.abhijitsarkar.webservices.jaxws.security.client;
 
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -67,19 +70,43 @@ public class CalculatorResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public int addUsingCalculatorEJB(@QueryParam(value = "arg1") int i,
 			@QueryParam(value = "arg2") int j) {
-		
+		final String username = "bob";
+		final String password = "password";
+
 		CalculatorEJB calcEJB = calculatorEJBService.getCalculatorEJB();
 
 		BindingProvider prov = (BindingProvider) calcEJB;
 
-		prov.getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "bob");
+		prov.getRequestContext().put(BindingProvider.USERNAME_PROPERTY,
+				username);
 		prov.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY,
-				"password");
+				password);
+
+		/*
+		 * This works too but the JAX-WS preferred way of basic auth is as above
+		 */
+		// Authenticator.setDefault(new SimpleAuthenticator(username,
+		// password));
 
 		int sum = calcEJB.add(i, j);
 
 		logger.info("Sum: " + sum);
 
 		return sum;
+	}
+
+	final class SimpleAuthenticator extends Authenticator {
+		private final String username;
+		private final String password;
+
+		SimpleAuthenticator(final String username, final String password) {
+			this.username = username;
+			this.password = password;
+		}
+
+		@Override
+		public PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(username, password.toCharArray());
+		}
 	}
 }
