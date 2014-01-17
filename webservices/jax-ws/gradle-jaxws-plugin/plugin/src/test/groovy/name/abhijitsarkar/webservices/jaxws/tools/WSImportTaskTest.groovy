86d8017b3d5e0c5d1a47b93d682713c9d30eb6da
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2014-01-17 18:32:13 -0500

    Deleted dead code

--- webservices/jax-ws/gradle-jaxws-plugin/plugin/src/test/groovy/name/abhijitsarkar/webservices/jaxws/tools/WSImportTaskTest.groovy
+++ webservices/jax-ws/gradle-jaxws-plugin/plugin/src/test/groovy/name/abhijitsarkar/webservices/jaxws/tools/WSImportTaskTest.groovy
@@ -6,11 +6,6 @@ import org.junit.Test
 
 class WSImportTaskTest {
 	private final Project project
-	//	private static final String userHome = System.getProperty("user.home")
-	//	private static final String GRADLE_INSTALLATION = userHome + File.separator +
-	//	"Applications" + File.separator + "gradle-default"
-	//	private static final String GRADLE_USER_HOME = userHome + File.separator + "Repositories" +
-	//	File.separator + "gradle"
 
 	WSImportTaskTest() {
 		ProjectBuilder projectBuilder = ProjectBuilder.builder()
@@ -19,24 +14,6 @@ class WSImportTaskTest {
 
 		project = projectBuilder.build()
 
-		//		project.sourceCompatibility = 1.7
-		//		project.group = "name.abhijitsarkar.webservices.jaxws.tools"
-		//		project.version = "1.0-SNAPSHOT"
-		//
-		//		project.apply plugin: "groovy"
-		//
-		//		project.repositories {
-		//			mavenCentral()
-		//			mavenLocal()
-		//		}
-		//
-		//		def cxfVersion = "2.7.8"
-		//
-		//		project.dependencies {
-		//			delegate.compile gradleApi()
-		//			delegate.compile group: "org.apache.cxf", name: "cxf-tools-wsdlto-core", version: "$cxfVersion"
-		//		}
-
 		project.task(type: WSImportTask, "wsimport")
 	}
 
@@ -44,45 +21,4 @@ class WSImportTaskTest {
 	public void testWsimportTaskAdded() {
 		assert project.tasks.findByName("wsimport") != null
 	}
-
-	//	@Test
-	//	public void testConfigure() {
-	//		Task wsimport = project.tasks.findByName("wsimport")
-	//
-	//		assert wsimport.WSDLToJavaClass == WSDLToJava.class
-	//		assert !wsimport.classpath.isEmpty()
-	//		assert wsimport.args.isEmpty()
-	//
-	//		testInitWsdlUrls(wsimport);
-	//	}
-
-	//	@Test
-	//	public void testWsimport() {
-	//		ProjectConnection connection = newProjectConnection();
-	//
-	//		try {
-	//			connection.newBuild().forTasks("WSImportTask").run();
-	//		} finally {
-	//			connection.close();
-	//		}
-	//	}
-
-	//	private ProjectConnection newProjectConnection() {
-	//		GradleConnector.newConnector()
-	//				.forProjectDirectory(new File("."))
-	//				.useInstallation(new File(GRADLE_INSTALLATION))
-	//				.useGradleUserHomeDir(new File(GRADLE_USER_HOME))
-	//				.connect();
-	//	}
-
-	//	private void testInitWsdlUrls(Task wsimport) {
-	//		String wsdlFile = new File(project.rootDir, "src/test/resources").toURI().toURL().toString() +
-	//				"AWSECommerceService.wsdl"
-	//
-	//		List<String> wsdlUrls = wsimport.wsdlUrls
-	//
-	//		assert wsdlUrls != null
-	//		assert wsdlUrls.size() == 1
-	//		assert wsdlUrls[0].toString() == wsdlFile
-	//	}
 }
