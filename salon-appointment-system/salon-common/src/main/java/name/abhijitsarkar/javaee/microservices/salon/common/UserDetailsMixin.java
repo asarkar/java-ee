359b3ffdc295e38b3af967c1b3abcb624c2390d2
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-10-25 01:21:50 -0700

    Trying to remove weird characters

--- salon-appointment-system/salon-common/src/main/java/name/abhijitsarkar/javaee/microservices/salon/common/UserDetailsMixin.java
+++
@@ -1,8 +0,0 @@
-package name.abhijitsarkar.javaee.microservices.salon.common;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-@JsonDeserialize(using = UserDetailsDeserializer.class)
-public abstract class UserDetailsMixin {
-
-}
