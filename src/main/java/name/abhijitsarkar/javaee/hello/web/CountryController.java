263a382ee899cd579b2ca3f90b1bc2d2e2525659
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-12-14 16:41:10 -0800

    Committing pagination code

--- src/main/java/name/abhijitsarkar/javaee/hello/web/CountryController.java
+++ src/main/java/name/abhijitsarkar/javaee/hello/web/CountryController.java
@@ -3,10 +3,11 @@ package name.abhijitsarkar.javaee.hello.web;
 import name.abhijitsarkar.javaee.hello.domain.Country;
 import name.abhijitsarkar.javaee.hello.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -21,7 +22,8 @@ public class CountryController {
     private CountryRepository countryRepo;
 
     @RequestMapping
-    public List<Country> findAll() {
-        return countryRepo.findAll();
+    public Page<Country> findAll(@RequestParam(defaultValue = "1") int pageNum,
+                                 @RequestParam(defaultValue = "10") int pageSize) {
+        return countryRepo.findAll(new PageRequest(pageNum - 1, pageSize));
     }
 }
