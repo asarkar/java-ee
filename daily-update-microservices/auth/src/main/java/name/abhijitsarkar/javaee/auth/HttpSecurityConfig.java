31b1e34e2f972407008b499d4bb894d8a9491606
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-12-29 16:35:33 -0800

    Started on an auth module

---
+++ daily-update-microservices/auth/src/main/java/name/abhijitsarkar/javaee/auth/HttpSecurityConfig.java
@@ -0,0 +1,10 @@
+package name.abhijitsarkar.javaee.auth;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+/**
+ * @author Abhijit Sarkar
+ */
+@EnableZuulServer
+public class HttpSecurityConfig {
+}
